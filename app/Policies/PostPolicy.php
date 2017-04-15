fdbf2c7d6619dbf908a431a7d2ebfdb44125a29c
sb1169693813 <446352377@qq.com>
2017-04-15 23:42:11 +0800

    3 complet

--- app/Policies/PostPolicy.php
+++ app/Policies/PostPolicy.php
@@ -30,5 +30,5 @@ class PostPolicy
     {
 
     }
-    
+
 }
--- database/migrations/2017_04_15_152821_create_roles_table.php
+++ database/migrations/2017_04_15_152821_create_roles_table.php
@@ -14,6 +14,42 @@ class CreateRolesTable extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');//admin member
+            $table->string('label')->nullable();
+            $table->timestamps();
+        });
+        Schema::create('permissions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('label')->nullable();
+            $table->timestamps();
+        });
+        Schema::create('permission_role', function (Blueprint $table) {
+            $table->integer('permission_id')->unsigned();
+            $table->integer('role_id')->unsigned;
+            $table->foreign('permission_id')
+                ->references('id')
+                ->on('permissions')
+                ->onDelete('cascade');
+            $table->foreign('role_id')
+                ->references('id')
+                ->on('roles')
+                ->onDelete('cascade');
+            $table->primary(['permission_id','role_id']);
+            $table->timestamps();
+        });
+        Schema::create('role_user', function (Blueprint $table) {
+            $table->integer('user_id')->unsigned();
+            $table->integer('role_id')->unsigned;
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+            $table->foreign('role_id')
+                ->references('id')
+                ->on('roles')
+                ->onDelete('cascade');
+            $table->primary(['user_id','role_id']);
             $table->timestamps();
         });
     }
